25fe20e340589618b28866aa5356ff54d3aa98e5
KeriCKelly <102998143+KeriCKelly@users.noreply.github.com>
2022-05-25 12:29:45 -0700

    fix js bugs
    Circletype linked

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // thank you https://circletype.labwire.ca/ for the circular type!!!
+console.log("ok");
 
-import CircleType from "circletype.min.js";
+// import CircleType from "https://cdnjs.cloudflare.com/ajax/libs/two.js/0.8.7/two.min.js";
 
 // Instantiate `CircleType` with an HTML element.
 const circleType = new CircleType(document.getElementById('myElement'));
 
 // Set the text radius and direction. Note: setter methods are chainable.
 circleType.radius(200).dir(1);
-
